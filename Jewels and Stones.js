5957fec3f9f978bbd9958a360b76163e1a714d51
superman285 <superman285@126.com>
2019-04-11 23:02:11 +0800

    jewels and stones leetcode algorithm use split('') to transform strings to array use filter to get the new arr use indexOf to judge whether the char is in the strings

---
+++ Jewels and Stones.js
@@ -0,0 +1,40 @@
+/*
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have. Each character in S is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+
+// my ugly js
+function numJewelsInStones(J,S) {
+    let arr = [];
+    for (let sletter of S) {
+        for (let jletter of J) {
+            if (sletter===jletter) {
+                arr.push(jletter);
+            }
+        }
+    }
+    return arr.length;
+}
+
+
+
+//cool js on leetcode
+const numJewelsInStones = (J,S)=>{
+    return S.split('').filter(sletter=>J.indexOf(sletter)!==-1).length;
+}
+
+/*const numJewelsInStones = (J,S)=>{
+    let S_arr = S.split('');
+   console.log(S_arr.map(letter=>{
+        return J.indexOf(letter)!==-1
+    }))
+}*/
+
+numJewelsInStones('as','asfbasfbasdf');
