2e0caeadef7be13ccfcbb8b55fa666c4d5f95c18
superman285 <superman285@126.com>
2019-04-16 19:42:02 +0800

    way2

--- Merge-two-binary-trees.js
+++ Merge-two-binary-trees.js
@@ -20,4 +20,17 @@ var mergeTrees = function(t1, t2) {
 
     return t1||t2;
 
+    //way2
+    /*let t;
+
+    if(t1 && t2) {
+        t = new TreeNode(t1.val + t2.val)
+        t.left = mergeTrees(t1.left, t2.left);
+        t.right = mergeTrees(t1.right, t2.right);
+    } else {
+        t = t1 || t2;
+    }
+
+    return t;*/
+
 };
