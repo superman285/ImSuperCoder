19cd1ac4fe3f76075865fa4447e0c930725b9f4d
superman285 <superman285@126.com>
2020-10-23 19:36:32 +0800

    递归思路 还不熟悉

--- 2020.10/顺时针打印矩阵.js
+++ 2020.10/顺时针打印矩阵.js
@@ -43,21 +43,79 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 * */
 
+// 题解: https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/solution/shou-hui-tu-jie-liang-chong-bian-li-de-ce-lue-na-c/
+
+
+// 思路对了 但是判断方式没想出来 搞那么复杂。
+
+/*
+* 1 2 3 4 5
+* 4 3 2 1 6
+* 1 2 3 4 7
+* 4 3 2 1 8
+* 1 2 3 4 9
+*
+* */
 /**
  * @param {number[][]} matrix
  * @return {number[]}
 	 1  2  3  4
 	 5  6  7  8
 	 9 10 11 12
+  13 14 15 16
 [0][0]~[0][3] [0][3]~[2][3] [2][3]~[2][0] [2][0]~[1][0] [1][0]~[1][2]
  */
-var spiralOrder = function(matrix) {
-
+let matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+var spiralOrder = function (matrix2) {
 	let row = 0, col = 0;
-	let rowMax = 4, colMax = 4;
+	const rowMax = 4
+	const colMax = 4
 	let ret = [];
-	let flags = matrix.map(arr => arr.map(num => 0))
-	while (ret.length!==matrix.length) {
+	let rowsFlag = 0, colsFlag = 0;
+	function walk(startRow = 0, startCol = 0, rowMax, colMax) {
+		// 0  1  2
+		for (let col = startCol; col < (colMax - colsFlag) + startCol; col++) {
+			// console.log('col ',col,matrix[rowsFlag][col]);
+			ret.push(matrix[rowsFlag - startCol][col])
+		}
+		rowsFlag++
+		startRow++
+		// 1
+		//for (let row = startRow; row < rowMax - (rowsFlag - 1); row++) {
+		for (let row = startRow; row < rowMax - (rowsFlag - startRow); row++) {
+			// console.log('row',startRow,colMax-1-colsFlag);
+			ret.push(matrix[row][colMax - 1 - colsFlag/2])
+		}
+		colsFlag++
+		startCol = (colMax - 1) - colsFlag + Math.floor(colsFlag/2)
+		// 改
+		console.log('startCol',startCol,colsFlag - 1);
+		for (let col = startCol; col >= (colsFlag - 1) /* ？- (colMax - startCol)*/; col--) {
+			console.log('看 rowsFlag,col',rowsFlag,col,matrix[rowMax-rowsFlag][col]);
+			ret.push(matrix[rowMax-rowsFlag][col])
+		}
+		startRow = (rowMax - 1) - rowsFlag
+		rowsFlag++
+		// 1
+		for (let row = startRow; row >= (rowsFlag - 1); row--) {
+			ret.push(matrix[row][colsFlag - 1])
+		}
+		startCol = (colsFlag - 1) + 1
+		colsFlag++
+
+		console.log('ret',ret, ret.length, matrix.flat(Infinity).length);
+
+		if (ret.length !== matrix.flat(Infinity).length) {
+			console.log('递归');
+			console.log(startRow,startCol,rowMax,colMax,rowsFlag,colsFlag);
+			// walk(startRow,startCol,rowMax,colMax)
+		}
+
+	}
+
+	walk(0,0,4,4)
+	walk(1,1,4,4)
+	/*while (ret.length !== matrix.length) {
 		// 右 ->
 		ret.push(matrix[row][col])
 		col++
@@ -73,6 +131,8 @@ var spiralOrder = function(matrix) {
 		// 右 ->
 		ret.push(matrix[row][col])
 		col++
-	}
+	}*/
 
 };
+
+spiralOrder(matrix)
